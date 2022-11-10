2fd179b6f7b714253d64fee415daa5b439e33864
cayomol01 <60375814+cayomol01@users.noreply.github.com>
2022-11-09 23:34:40 -0600

    Inciso 5

---
+++ Corto3_5.py
@@ -0,0 +1,8 @@
+
+a = ['rojo', 'verde', 'azul', 'amarillo', 'gris', 'blanco', 'negro']
+b = ['amarillo', 'cafe', 'blanco']
+
+
+filterOut = lambda lista, retirar: [element for element in lista if element not in retirar]
+
+
