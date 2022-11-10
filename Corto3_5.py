ec2e29fcdf7b980f214dbdd11f8309bf442f4f30
cayomol01 <60375814+cayomol01@users.noreply.github.com>
2022-11-10 00:01:25 -0600

    Inciso 4 y 2

--- Corto3_5.py
+++ Corto3_5.py
@@ -2,7 +2,14 @@
 a = ['rojo', 'verde', 'azul', 'amarillo', 'gris', 'blanco', 'negro']
 b = ['amarillo', 'cafe', 'blanco']
 
-
+#inciso 5
 filterOut = lambda lista, retirar: [element for element in lista if element not in retirar]
 
 
+#Inciso 4
+reverse = lambda lista: [element[::-1] for element in lista]
+print(reverse(a))
+
+#Inciso 2
+power = lambda lista, pwr: [element**pwr for element in lista]
+c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
